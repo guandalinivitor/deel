52492fcccfc4aa183373cfa6611a384d5a1d12ea
guandalinivitor <guandalinivitor@gmail.com>
2022-11-15 00:10:21 -0300

    Editing Readme and finalize exercise.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
+## Hey, Vitor here 👨🏼‍💻, who made this test. Hope to get an answer from you guys. Will be waiting.
 
+## Some texts to get in touch about how I solved the questions. 🙏🏼
+
+changes on server.js -> making sure the db is up and running 
+
+using nvm to manage node version 
+
+Sequelize -> Never worked with this framework before, but really good explanation on website and docs. 
+
+Didn't get enough time to make unit test and front-end calling the api. 
+But if I get hired I will do my best working on BE and try to help on FE as well as I have been working as fullstack
+
+On the end of the test it started to get confused, guess we need to explain a bit better how to get the values imho.
+
+there are some comments on the code that helps explain my thoughts.
--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const {sequelize, Profile, Contract, Job} = require('./model')
 const {getProfile} = require('./middleware/getProfile')
-const { Op } = require("sequelize")
+const { Op, ENUM } = require("sequelize")
 const app = express();
 app.use(bodyParser.json());
 app.set('sequelize', sequelize)
@@ -54,8 +54,9 @@ app.get('/contracts', getProfile, async (req, res) =>{
         [Op.and]: [{status: 'in_progress'}] 
     }})
 
+    //creating an array only with ids to pass on the condition IN 
     contract.forEach(element => {
-        if (element.dataValues.id) { 
+        if (element.dataValues?.id) { 
             arrayIds.push(element.dataValues.id);
         }    
     });
@@ -80,23 +81,66 @@ app.get('/contracts', getProfile, async (req, res) =>{
  */
  app.post('/jobs/:job_id/pay', getProfile, async (req, res) =>{
     const {Job} = req.app.get('models')
+    const {Profile} = req.app.get('models')
     const {Contract} = req.app.get('models')
+    const balance = req.profile.balance
+    const payment = req.body
 
-    if(!jobs) return res.status(404).end()
-    res.json(jobs)
+    //get the job id by parameter
+    //get the contractId to identify which contract is for that job
+    //get the contractor by the contractorId on the contract 
+    //verify and move money
+    
+    const {job_id} = req.params
+    const jobs = await Job.findOne({ where: {
+        id: { 
+            [Op.eq]: job_id
+        }
+    }})
+
+    const contractId = jobs.ContractId;
+    const contract = await Contract.findOne({ where: {
+        id: { 
+            [Op.eq]: contractId
+        }
+    }})
+
+    const contractorId = contract.ContractorId;
+    const contractorToBePaid = await Profile.findOne({ where: { 
+        id: { 
+            [Op.eq]: contractorId
+        }
+    }})
+
+    //I get confused what do I have to verify here on the body to get the payment
+    if (contractorToBePaid.type === 'contractor') { 
+        if (contractorToBePaid.balance >= payment.paid) {
+            await Profile.update({balance: balance}, { 
+                where: { 
+                    id: {
+                        [Op.eq]: contractorId
+                    }
+                }
+            })
+        }
+    }
+    
+    return res.status(200).end()
 });
 
 
 /** 5
- * Deposits money into the the the balance of a client, a client can't deposit 
+ * Deposits money into the balance of a client, a client can't deposit <- didn't understanding what is asked here
  * more than 25% his total of jobs to pay. (at the deposit moment)
  */
  app.post('/balances/deposit/:userId', getProfile, async (req, res) =>{
+    //Now I get pretty confused, the client id should be the :userId, profile_id(header) or id(json request)? 
     const {Job} = req.app.get('models')
+    const {Profile} = req.app.get('models')
     const {Contract} = req.app.get('models')
+    const {userId} = req.params
 
-    if(!jobs) return res.status(404).end()
-    res.json(jobs)
+    return res.status(501).end()
 });
 
 
@@ -104,25 +148,132 @@ app.get('/contracts', getProfile, async (req, res) =>{
  * @returns The profession that earned the most money (sum of jobs paid) for 
  * any contactor that worked in the query time range.
  */
- app.get('/admin/best-profession?start=<date>&end=<date>', getProfile, async (req, res) =>{
+ app.get('/admin/best-profession', getProfile, async (req, res) =>{
     const {Job} = req.app.get('models')
+    const {Profile} = req.app.get('models')
     const {Contract} = req.app.get('models')
+    const { startDate, endDate } = req.query
+    let arrayIds = []
+    let arrayContractors = []
+    let arrayJobs = []
 
-    if(!jobs) return res.status(404).end()
-    res.json(jobs)
+    const onlyContractors = await Profile.findAll({ where: { 
+        type: { 
+            [Op.eq]: 'contractor'
+        }
+    }})
+
+    onlyContractors.forEach(element => {
+        if (element.dataValues?.id) { 
+            arrayContractors.push(element.dataValues.id);
+        }    
+    });
+
+    const contract = await Contract.findAll({ where: {
+        ContractorId: { 
+            [Op.in]: arrayContractors
+        }
+    }})
+
+    contract.forEach(element => {
+        if (element.dataValues?.id) { 
+            arrayIds.push(element.dataValues.id);
+        }    
+    });
+
+    //here I have all paid jobs made by contractors
+    const jobs = await Job.findAll({ where: {
+        paid: { 
+            [Op.eq]: true,  
+        },
+        ContractId: { 
+            [Op.in]: arrayIds
+        },
+        paymentDate: { 
+            [Op.between]: [startDate, endDate]
+        }
+    }})
+
+    jobs.forEach(element => {
+        if (element.dataValues?.id) { 
+            arrayJobs.push(element.dataValues.id);
+        }    
+    });
+
+    const profession = await Profile.findAll({ where: { 
+        id: { 
+            [Op.in]: arrayJobs
+        }
+    },
+        //sum of all the jobs filtered
+        attributes: [
+            'profession',
+            [sequelize.fn('sum', sequelize.col('balance')), 'total_balance'],
+        ],   
+        group: ['profession'],
+        raw: true
+    })
+
+    if(!profession) return res.status(404).end()
+    res.json(profession)
 });
 
 
 /** 7
- * @returns the clients the paid the most for jobs in the query time period. 
+ * @returns The clients the paid the most for jobs in the query time period. 
  * limit query parameter should be applied, default limit is 2.
  */
- app.get('/admin/best-clients?start=<date>&end=<date>&limit=<integer>', getProfile, async (req, res) =>{
+ app.get('/admin/best-clients', getProfile, async (req, res) =>{
     const {Job} = req.app.get('models')
+    const {Profile} = req.app.get('models')
     const {Contract} = req.app.get('models')
+    const { startDate, endDate, limit } = req.query
+    let arrayContracts = []
+    let arrayClients = []
 
-    if(!jobs) return res.status(404).end()
-    res.json(jobs)
+    const jobs = await Job.findAll({ where: {
+        paid: { 
+            [Op.eq]: true,  
+        },
+        paymentDate: { 
+            [Op.between]: [startDate, endDate]
+        },
+    },
+        attributes: [
+            'ContractId', 'paid', 'paymentDate',
+            [sequelize.fn('max', sequelize.col('price')), 'max_amount_paid']
+        ],
+        group: ['ContractId', 'paid', 'paymentDate'],
+        raw: true,
+        limit: limit,
+    })
+
+    jobs.forEach(element => {
+        if (element.ContractId) { 
+            arrayContracts.push(element.ContractId);
+        }    
+    });
+
+    const contract = await Contract.findAll({ where: {
+        id: { 
+            [Op.in]: arrayContracts
+        }
+    }})
+
+    contract.forEach(element => {
+        if (element.dataValues?.ClientId) { 
+            arrayClients.push(element.dataValues?.ClientId);
+        }    
+    });
+
+    const clientsPaidMost = await Profile.findAll({ where: { 
+        id: { 
+            [Op.in]: arrayClients
+        }
+    }})
+
+    if(!clientsPaidMost) return res.status(404).end()
+    res.json(clientsPaidMost)
 });
 
 module.exports = app;
